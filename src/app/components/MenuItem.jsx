c80d7b6136e940cb5b2966e848ee9feb45383e5c
salih-kv <profileofsalih@gmail.com>
2024-01-02 10:34:44 +0530

    handle the error

--- src/app/components/MenuItem.jsx
+++ src/app/components/MenuItem.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 const MenuItem = ({ title, path, Icon }) => {
   return (
     <div>
-      <Link href={path} className="mx-4 lg:mx-6 hover:text-amber-600">
+      <Link href={path} className="mx-4 hover:text-amber-600">
         <Icon className="text-2xl sm:hidden mx-4" />
-        <p className="hidden sm:inline my-2 text-sm">{title}</p>
+        <p className="hidden sm:inline my-2 text-sm font-medium">{title}</p>
       </Link>
     </div>
   );
